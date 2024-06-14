a84a55261fa9227ea720e474ab4a2ef4176025e7
hugo duchene <hugo4.dumalaco@gmail.com>
2024-06-14 11:29:20 +0200

    first commit

--- App.py
+++ App.py
@@ -9,7 +9,18 @@ ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king', 
 class App(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
+        self.title('Blackjack Expert')
         self.deck = Deck()
+
+        self.table_image = Image.open('design/bj_background.png')
+        self.table_photo = ImageTk.PhotoImage(self.table_image)
+
+        self.canvas = tk.Canvas(self.table_photo, width=self.table_photo.width(), height=self.table_photo.height())
+        self.canvas.pack()
+
+        self.canvas.create_image(0, 0, image=self.table_photo)
+
+        self.display_card()
 
     def draw_card_path(self):
         drawn_cards, self.deck = self.deck.draw_cards(1)
